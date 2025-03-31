94e08e8d4e9fdbb5967144012fa7c925df246992
Ovejero-Sayavedra-Mateo-Leon <mlovejerosayavedra@mail.austral.edu.ar>
2025-03-31 14:50:58 -0300

    earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,7 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    comparar = x < y
+    comparar_2 = x > y
+    print(f"The result of {x} comes first in the dictionary than {y} is {comparar}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {comparar_2}.")
